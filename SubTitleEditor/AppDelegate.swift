402fcf76f38447a90b6bf12514c16c83c934d975
장기현 <nuky99@naver.com>
2015-12-15 15:31:28 +0900

    Initial Commit

---
+++ SubTitleEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SubTitleEditor
+//
+//  Created by 장기현 on 2015. 12. 15..
+//  Copyright © 2015년 JKH. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SubTitleEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SubTitleEditor
+//
+//  Created by 장기현 on 2015. 12. 15..
+//  Copyright © 2015년 JKH. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
