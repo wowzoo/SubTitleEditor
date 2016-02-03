52faea745170f6364afcde373c3319e41647f576
wowzoo <wowzoo@gmail.com>
2016-02-03 13:57:38 +0900

    SubTitleTimeIntervalCell now inherits NSTextFieldCell

--- SubTitleEditor/SubTitleTimeintervalPicker.swift
+++ SubTitleEditor/SubTitleTimeintervalPicker.swift
@@ -8,40 +8,45 @@
 
 import Cocoa
 
-class SubTitleTimeIntervalCell {
-    let cell: NSTextFieldCell
-    let size: NSSize
-    let max: Int
-    let formatString: String
-    let limit: Int
+class SubTitleTimeIntervalCell: NSTextFieldCell {
+    let size: NSSize?
+    let max: Int?
+    let formatString: String?
+    let limit: Int?
     
     var right: SubTitleTimeIntervalCell?
     var left: SubTitleTimeIntervalCell?
     
     var frame: NSRect?
     
-    var integerValue: Int = 0 {
+    override var integerValue: Int {
         didSet {
-            self.cell.stringValue = String(format: self.formatString, integerValue)
+            super.stringValue = String(format: self.formatString!, integerValue)
         }
     }
     
-    var stringValue: String {
-        get {
-            return self.cell.stringValue
-        }
-    }
-    
-    init(cell: NSTextFieldCell, size: NSSize, max: Int, formatString: String, limit: Int) {
-        self.cell = cell
+    init(textCell: String, size: NSSize, max: Int, formatString: String?, limit: Int) {
         self.size = size
         self.max = max
         self.formatString = formatString
         self.limit = limit
+        
+        super.init(textCell: textCell)
     }
-    
-    func drawWithFrame(controlView: NSView) {
-        self.cell.drawWithFrame(frame!, inView: controlView)
+
+    required init?(coder aDecoder: NSCoder) {
+        self.size = nil
+        self.max = nil
+        self.formatString = nil
+        self.limit = nil
+        
+        super.init(coder: aDecoder)
+    }
+   
+    override func drawWithFrame(cellFrame: NSRect, inView controlView: NSView) {
+        super.drawWithFrame(cellFrame, inView: controlView)
+        
+        self.frame = cellFrame
     }
     
     func updateValue(amount: Int) -> String {
@@ -70,21 +75,14 @@ class SubTitleTimeIntervalCell {
 
 @IBDesignable
 class SubTitleTimeintervalPicker: NSControl, NSTextViewDelegate {
-    var hoursComponent: SubTitleTimeIntervalCell?
-    var minutesComponent: SubTitleTimeIntervalCell?
-    var secondsComponent: SubTitleTimeIntervalCell?
-    var millisecondsComponent: SubTitleTimeIntervalCell?
+    var hoursCell: SubTitleTimeIntervalCell?
+    var minutesCell: SubTitleTimeIntervalCell?
+    var secondsCell: SubTitleTimeIntervalCell?
+    var millisecondsCell: SubTitleTimeIntervalCell?
     
-    var separatorCell1: NSTextFieldCell?
-    var separatorCellFrame1: NSRect?
-    
-    var separatorCell2: NSTextFieldCell?
-    var separatorCellFrame2: NSRect?
-    
-    var separatorCell3: NSTextFieldCell?
-    var separatorCellFrame3: NSRect?
-    
-    let separatorCellSize: NSSize = NSSize(width: 5, height: 15)
+    var separatorCell1: SubTitleTimeIntervalCell?
+    var separatorCell2: SubTitleTimeIntervalCell?
+    var separatorCell3: SubTitleTimeIntervalCell?
     
     var stepperCell: NSStepperCell?
     var stepperCellFrame: NSRect?
@@ -95,7 +93,7 @@ class SubTitleTimeintervalPicker: NSControl, NSTextViewDelegate {
     
     var intervalTimeString: String {
         get {
-            return "\(self.hoursComponent!.stringValue):\(self.minutesComponent!.stringValue):\(self.secondsComponent!.stringValue),\(self.millisecondsComponent!.stringValue)"
+            return "\(self.hoursCell!.stringValue):\(self.minutesCell!.stringValue):\(self.secondsCell!.stringValue),\(self.millisecondsCell!.stringValue)"
         }
     }
     
@@ -110,19 +108,19 @@ class SubTitleTimeintervalPicker: NSControl, NSTextViewDelegate {
     }
     
     func setUpCells() {
-        self.hoursComponent = SubTitleTimeIntervalCell(cell: self.setupCellWithString("00", alignment: .Right), size: NSSize(width: 19, height: 15), max: 24, formatString: "%02d", limit: 10)
-        self.minutesComponent = SubTitleTimeIntervalCell(cell: self.setupCellWithString("00", alignment: .Right), size: NSSize(width: 19, height: 15), max: 60, formatString: "%02d", limit: 10)
-        self.secondsComponent = SubTitleTimeIntervalCell(cell: self.setupCellWithString("00", alignment: .Right), size: NSSize(width: 19, height: 15), max: 60, formatString: "%02d", limit: 10)
-        self.millisecondsComponent = SubTitleTimeIntervalCell(cell: self.setupCellWithString("000", alignment: .Right), size: NSSize(width: 27, height: 15), max: 999, formatString: "%03d", limit: 100)
+        self.hoursCell = setupTimeIntervalCell("00", alignment: .Right, size: NSSize(width: 19, height: 15), max: 24, formatString: "%02d", limit: 10)
+        self.minutesCell = setupTimeIntervalCell("00", alignment: .Right, size: NSSize(width: 19, height: 15), max: 60, formatString: "%02d", limit: 10)
+        self.secondsCell = setupTimeIntervalCell("00", alignment: .Right, size: NSSize(width: 19, height: 15), max: 60, formatString: "%02d", limit: 10)
+        self.millisecondsCell = setupTimeIntervalCell("000", alignment: .Right, size: NSSize(width: 27, height: 15), max: 999, formatString: "%03d", limit: 100)
         
-        self.hoursComponent!.setNeighbors(nil, right: self.minutesComponent)
-        self.minutesComponent!.setNeighbors(self.hoursComponent, right: self.secondsComponent)
-        self.secondsComponent!.setNeighbors(self.minutesComponent, right: self.millisecondsComponent)
-        self.millisecondsComponent!.setNeighbors(self.secondsComponent, right: nil)
+        self.hoursCell!.setNeighbors(nil, right: self.minutesCell)
+        self.minutesCell!.setNeighbors(self.hoursCell, right: self.secondsCell)
+        self.secondsCell!.setNeighbors(self.minutesCell, right: self.millisecondsCell)
+        self.millisecondsCell!.setNeighbors(self.secondsCell, right: nil)
         
-        self.separatorCell1 = self.setupCellWithString(":", alignment: .Left)
-        self.separatorCell2 = self.setupCellWithString(":", alignment: .Left)
-        self.separatorCell3 = self.setupCellWithString(",", alignment: .Left)
+        self.separatorCell1 = self.setupTimeIntervalCell(":", alignment: .Left, size: NSSize(width: 5, height: 15))
+        self.separatorCell2 = self.setupTimeIntervalCell(":", alignment: .Left, size: NSSize(width: 5, height: 15))
+        self.separatorCell3 = self.setupTimeIntervalCell(",", alignment: .Left, size: NSSize(width: 5, height: 15))
         
         self.stepperCell = NSStepperCell()
     }
@@ -133,13 +131,12 @@ class SubTitleTimeintervalPicker: NSControl, NSTextViewDelegate {
         self.addTrackingArea(trackingArea)
     }
     
-    func setupCellWithString(string: String, alignment: NSTextAlignment) -> NSTextFieldCell {
-        let cell: NSTextFieldCell = NSTextFieldCell(textCell: string)
+    func setupTimeIntervalCell(string: String, alignment: NSTextAlignment, size: NSSize, max: Int = 0, formatString: String? = nil, limit: Int = 0) -> SubTitleTimeIntervalCell {
+        let cell: SubTitleTimeIntervalCell = SubTitleTimeIntervalCell(textCell: string, size: size, max: max, formatString: formatString, limit: limit)
         cell.drawsBackground = true
-        cell.backgroundColor = NSColor.lightGrayColor()
+        cell.backgroundColor = NSColor.whiteColor()
         cell.editable = true
         cell.bordered = false
-        //cell.controlSize = .SmallControlSize
         cell.font = NSFont.controlContentFontOfSize(NSFont.systemFontSizeForControlSize(.SmallControlSize))
         cell.alignment = alignment
         return cell
@@ -150,72 +147,57 @@ class SubTitleTimeintervalPicker: NSControl, NSTextViewDelegate {
         //Swift.print("drawRect")
         
         // Draw Control Frame
-        var borderFrame: NSRect = self.bounds
-        borderFrame.size.width -= self.stepperCell!.cellSize.width - 2.0
+        var controlFrame: NSRect = self.bounds
+        controlFrame.size.width -= self.stepperCell!.cellSize.width - 2.0
         
-        NSColor.lightGrayColor().setFill()
-        NSBezierPath(roundedRect: borderFrame, xRadius: 5.0, yRadius: 5.0).fill()
+        NSColor.whiteColor().setFill()
+        NSBezierPath(roundedRect: controlFrame, xRadius: 5.0, yRadius: 5.0).fill()
         
         // Draw Cells Frame
-        let baseFrame: NSRect = self.bounds
-        var frame: NSRect = baseFrame
+        var cellFrame: NSRect = self.bounds
         
         // stepper cell
-        frame.size = self.stepperCell!.cellSize
-        frame.origin.x = NSMaxX(baseFrame) - frame.size.width + 1.0
-        frame.origin.y -= 1.0
-        self.stepperCellFrame = frame
+        cellFrame.size = self.stepperCell!.cellSize
+        cellFrame.origin.x = NSMaxX(self.bounds) - cellFrame.size.width + 1.0
+        cellFrame.origin.y -= 1.0
+        self.stepperCellFrame = cellFrame
         self.stepperCell!.drawWithFrame(self.stepperCellFrame!, inView: self)
         
-        frame = NSInsetRect(baseFrame, 2.0, 3.0)
+        cellFrame = NSInsetRect(self.bounds, 2.0, 3.0)
         
         // hours cell
-        frame.size = self.hoursComponent!.size
-        
-        self.hoursComponent!.frame = frame
-        self.hoursComponent!.drawWithFrame(self)
+        cellFrame.size = self.hoursCell!.size!
+        self.hoursCell!.drawWithFrame(cellFrame, inView: self)
         
         // separator1
-        frame.origin.x += frame.size.width
-        frame.size = separatorCellSize
-        
-        self.separatorCellFrame1 = frame
-        self.separatorCell1!.drawWithFrame(self.separatorCellFrame1!, inView: self)
+        cellFrame.origin.x += cellFrame.size.width
+        cellFrame.size = self.separatorCell1!.size!
+        self.separatorCell1!.drawWithFrame(cellFrame, inView: self)
         
         // minutes cell
-        frame.origin.x += frame.size.width
-        frame.size = self.minutesComponent!.size
-        
-        self.minutesComponent!.frame = frame
-        self.minutesComponent!.drawWithFrame(self)
+        cellFrame.origin.x += cellFrame.size.width
+        cellFrame.size = self.minutesCell!.size!
+        self.minutesCell!.drawWithFrame(cellFrame, inView: self)
         
         // separator2
-        frame.origin.x += frame.size.width
-        frame.size = separatorCellSize
-        
-        self.separatorCellFrame2 = frame
-        self.separatorCell2!.drawWithFrame(self.separatorCellFrame2!, inView: self)
+        cellFrame.origin.x += cellFrame.size.width
+        cellFrame.size = self.separatorCell2!.size!
+        self.separatorCell2!.drawWithFrame(cellFrame, inView: self)
         
         // seconds cell
-        frame.origin.x += frame.size.width
-        frame.size = self.secondsComponent!.size
-        
-        self.secondsComponent!.frame = frame
-        self.secondsComponent!.drawWithFrame(self)
+        cellFrame.origin.x += cellFrame.size.width
+        cellFrame.size = self.secondsCell!.size!
+        self.secondsCell!.drawWithFrame(cellFrame, inView: self)
         
         // separator3
-        frame.origin.x += frame.size.width
-        frame.size = separatorCellSize
-        
-        self.separatorCellFrame3 = frame
-        self.separatorCell3!.drawWithFrame(self.separatorCellFrame3!, inView: self)
+        cellFrame.origin.x += cellFrame.size.width
+        cellFrame.size = self.separatorCell3!.size!
+        self.separatorCell3!.drawWithFrame(cellFrame, inView: self)
         
         // milliseconds cell
-        frame.origin.x += frame.size.width
-        frame.size = self.millisecondsComponent!.size
-        
-        self.millisecondsComponent!.frame = frame
-        self.millisecondsComponent!.drawWithFrame(self)
+        cellFrame.origin.x += cellFrame.size.width
+        cellFrame.size = self.millisecondsCell!.size!
+        self.millisecondsCell!.drawWithFrame(cellFrame, inView: self)
     }
     
     override func mouseDown(theEvent: NSEvent) {
@@ -233,7 +215,7 @@ class SubTitleTimeintervalPicker: NSControl, NSTextViewDelegate {
             self.stepperCell!.trackMouse(theEvent, inRect: self.bounds, ofView: self, untilMouseUp: true)
             
             guard let currentEditor = self.currentEditor() else {
-                selectComponent(self.secondsComponent!)
+                selectComponent(self.secondsCell!)
                 return
             }
             
@@ -241,29 +223,29 @@ class SubTitleTimeintervalPicker: NSControl, NSTextViewDelegate {
             self.selectedComponent!.integerValue = stepperValue
             currentEditor.string = self.selectedComponent!.stringValue
             
-        } else if NSMouseInRect(locationInControlFrame, self.hoursComponent!.frame!, self.flipped) {
-            selectComponent(self.hoursComponent!)
-        } else if NSMouseInRect(locationInControlFrame, self.minutesComponent!.frame!, self.flipped) {
-            selectComponent(self.minutesComponent!)
-        } else if NSMouseInRect(locationInControlFrame, self.secondsComponent!.frame!, self.flipped) {
-            selectComponent(self.secondsComponent!)
-        } else if NSMouseInRect(locationInControlFrame, self.millisecondsComponent!.frame!, self.flipped) {
-            selectComponent(self.millisecondsComponent!)
+        } else if NSMouseInRect(locationInControlFrame, self.hoursCell!.frame!, self.flipped) {
+            selectComponent(self.hoursCell!)
+        } else if NSMouseInRect(locationInControlFrame, self.minutesCell!.frame!, self.flipped) {
+            selectComponent(self.minutesCell!)
+        } else if NSMouseInRect(locationInControlFrame, self.secondsCell!.frame!, self.flipped) {
+            selectComponent(self.secondsCell!)
+        } else if NSMouseInRect(locationInControlFrame, self.millisecondsCell!.frame!, self.flipped) {
+            selectComponent(self.millisecondsCell!)
         }
     }
     
-    func selectComponent(component: SubTitleTimeIntervalCell) {
+    func selectComponent(cell: SubTitleTimeIntervalCell) {
         if let selectedComponent = self.selectedComponent {
-            selectedComponent.cell.endEditing(self.currentEditor()!)
+            selectedComponent.endEditing(self.currentEditor()!)
         }
         
-        self.selectedComponent = component
+        self.selectedComponent = cell
         
-        let fieldEditor: NSText! = self.window!.fieldEditor(true, forObject: component.cell)
+        let fieldEditor: NSText! = self.window!.fieldEditor(true, forObject: cell)
         let length = fieldEditor.string!.characters.count
-        component.cell.selectWithFrame(component.frame!, inView: self, editor: fieldEditor, delegate: self, start: 0, length: length)
+        cell.selectWithFrame(cell.frame!, inView: self, editor: fieldEditor, delegate: self, start: 0, length: length)
         
-        updateStepper(component.max, min: 0, value: component.cell.stringValue)
+        updateStepper(cell.max!, min: 0, value: cell.stringValue)
     }
     
     func textView(textView: NSTextView, shouldChangeTextInRange affectedCharRange: NSRange, replacementString: String?) -> Bool {
@@ -281,8 +263,9 @@ class SubTitleTimeintervalPicker: NSControl, NSTextViewDelegate {
         
         let addedValue: Int = Int(replacementString!)!
         let currentValue = self.selectedComponent!.updateValue(addedValue)
+        
         currentEditor.string = currentValue
-        updateStepper(self.selectedComponent!.max, min: 0, value: currentValue)
+        updateStepper(self.selectedComponent!.max!, min: 0, value: currentValue)
         
         return false
     }
@@ -333,8 +316,8 @@ class SubTitleTimeintervalPicker: NSControl, NSTextViewDelegate {
         
         if !self.mouseInTheArea {
             //Swift.print("Lost Focus")
-            if let component = self.selectedComponent {
-                component.cell.endEditing(self.currentEditor()!)
+            if let selectedComponent = self.selectedComponent {
+                selectedComponent.endEditing(self.currentEditor()!)
                 self.selectedComponent = nil
             }
         }
